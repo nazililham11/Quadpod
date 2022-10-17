827355be771ea922a2b3175d80f2beb82d432d2c
nazililham11 <nazil.ilham11@gmail.com>
2022-10-17 14:15:31 +0700

    Penambahan Serial Command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Servo.h>
 #include <ATmega128_pins.h>
+#include <CommandHandler.h>
 
 // Servo
 Servo servo[4][3];
@@ -11,10 +12,19 @@ const uint8_t servo_pin[4][3] = {
     {PORT_D_7, PORT_D_6, PORT_D_5},  // Belakang Kanan
     {PORT_B_7, PORT_B_6, PORT_B_5}}; // Belakang Kiri
 
+CommandHandler<> command(Serial, '[', ']');
+
+// Function
+void cmd_unknown();
+void cmd_set_servo(CommandParameter &parameters);
+
 void setup()
 {
     Serial.begin(115200);
     Serial.println("Setup");
+
+    command.AddCommand(F("servo"), cmd_set_servo);
+    command.SetDefaultHandler(cmd_unknown);
 
     for (uint8_t i = 0; i < 4; i++)
     {
@@ -28,6 +38,40 @@ void setup()
 
 void loop()
 {
-   
+    command.Process();
+}
+
+
+void cmd_unknown(){
+    Serial.println(F("Unknown Command"));
+}
+
+void cmd_set_servo(CommandParameter &params)
+{
+    int leg = params.NextParameterAsInteger();
+
+    if (leg < 0 || leg > 3) return;
+
+    Serial.print(F("Leg "));
+    Serial.print(leg);
+
+    const int angle[3] = {
+        params.NextParameterAsInteger(),
+        params.NextParameterAsInteger(),
+        params.NextParameterAsInteger(),
+    };
+
+    for (uint8_t i = 0; i < 3; i++){
+        if (angle[i] < 0) return;
+        if (angle[i] < 180) {
+            servo[leg][i].write(angle[i]);
+            Serial.print(F(" "));
+            Serial.print(angle[i]);
+        } else {
+            Serial.print(F(" KEEP"));
+        }
+    }
+
+    Serial.println();
 }
 
