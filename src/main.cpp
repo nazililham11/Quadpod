238c80d5846081ebffc5e22244f683e9c2a0ee7d
nazililham11 <nazil.ilham11@gmail.com>
2022-10-17 14:33:51 +0700

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,55 @@
-#include <Arduino.h>
-#include <Servo.h>
 #include <ATmega128_pins.h>
+#include <Arduino.h>
 #include <CommandHandler.h>
+#include <Servo.h>
 
 // Servo
 Servo servo[4][3];
 const uint8_t servo_pin[4][3] = {
     // Coxa,    Femur,    Tibia
-    {PORT_F_7, PORT_F_6, PORT_F_5},  // Depan Kiri
-    {PORT_A_0, PORT_A_1, PORT_A_2},  // Depan Kanan
-    {PORT_D_7, PORT_D_6, PORT_D_5},  // Belakang Kanan
-    {PORT_B_7, PORT_B_6, PORT_B_5}}; // Belakang Kiri
+    {PORT_F_7, PORT_F_6, PORT_F_5},   // Depan Kiri
+    {PORT_A_0, PORT_A_1, PORT_A_2},   // Depan Kanan
+    {PORT_D_7, PORT_D_6, PORT_D_5},   // Belakang Kanan
+    {PORT_B_7, PORT_B_6, PORT_B_5}};  // Belakang Kiri
 
 CommandHandler<> command(Serial, '[', ']');
 
 // Function
+void servo_init();
+
+void cmd_init();
 void cmd_unknown();
 void cmd_set_servo(CommandParameter &parameters);
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
     Serial.println("Setup");
 
-    command.AddCommand(F("servo"), cmd_set_servo);
-    command.SetDefaultHandler(cmd_unknown);
+    cmd_init();
+    servo_init();
+}
 
-    for (uint8_t i = 0; i < 4; i++)
-    {
-        for (uint8_t j = 0; j < 3; j++)
-        {
+void loop() { 
+    command.Process(); 
+}
+
+void servo_init() {
+    for (uint8_t i = 0; i < 4; i++) {
+        for (uint8_t j = 0; j < 3; j++) {
             servo[i][j].attach(servo_pin[i][j]);
             servo[i][j].write(90);
         }
     }
 }
 
-void loop()
-{
-    command.Process();
+void cmd_init() {
+    command.AddCommand(F("servo"), cmd_set_servo);
+    command.SetDefaultHandler(cmd_unknown);
 }
 
+void cmd_unknown() { Serial.println(F("Unknown Command")); }
 
-void cmd_unknown(){
-    Serial.println(F("Unknown Command"));
-}
-
-void cmd_set_servo(CommandParameter &params)
-{
+void cmd_set_servo(CommandParameter &params) {
     int leg = params.NextParameterAsInteger();
 
     if (leg < 0 || leg > 3) return;
@@ -61,7 +63,7 @@ void cmd_set_servo(CommandParameter &params)
         params.NextParameterAsInteger(),
     };
 
-    for (uint8_t i = 0; i < 3; i++){
+    for (uint8_t i = 0; i < 3; i++) {
         if (angle[i] < 0) return;
         if (angle[i] < 180) {
             servo[leg][i].write(angle[i]);
@@ -74,4 +76,3 @@ void cmd_set_servo(CommandParameter &params)
 
     Serial.println();
 }
-
